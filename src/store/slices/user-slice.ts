9e68e29d6450fdcca919c0a52f2f3754f201fa8c
Ali Raza <141334011+Ali-Esspfa@users.noreply.github.com>
2024-03-08 17:22:57 +0500

    Updated code

---
+++ src/store/slices/user-slice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialUserState = {
+  userInfo: {},
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: initialUserState,
+  reducers: {
+    catchError: (state: any, action: any) => {
+      state.error = `${action.type}: ${action.payload.error}`;
+      state.actionsLoading = false;
+    },
+    getUserData: (state, action) => {
+      state.userInfo = action.payload;
+    },
+  },
+});
+
+export const { getUserData } = userSlice.actions;
